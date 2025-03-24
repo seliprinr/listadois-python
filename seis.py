ac760c1f6bb396a6ac0e7231f0527f43a6b24905
Rodrigo Seliprin <rseliprin@gmail.com>
2025-03-24 20:53:06 -0300

    Exercícios em andamento

--- seis.py
+++ seis.py
@@ -1,2 +1,13 @@
 # Escrever um programa para ler uma string e copiar para dentro de uma outra string todas as
 # vogais. Exibir na tela a string formada pelas letras copiadas.
+
+texto = input("Digite uma string: ")
+
+vogais = "aeiouAEIOU"
+resultado = ""
+
+for letra in texto:
+    if letra in vogais:
+        resultado += letra
+
+print(f"String formada apenas pelas vogais: {resultado}")
